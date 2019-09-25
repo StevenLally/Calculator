f48892f6744310a55c22c750f32a4092cdbd4c3c
Steve Lally <steve.lally89@gmail.com>
2019-09-25 16:20:42 -0400

    add basic html/css work on DOM events

--- scripts/script.js
+++ scripts/script.js
@@ -30,30 +30,71 @@ function operate(operator, a, b){
     }
 }
 
-let arr = [2, "+", 6, "/", 6, "+", 2, "*", 3, "-", 5];
-let run = true;
+let arr = [2, "+", 6, "/", 6, "+", 2, "*", 3, "-", 5 , "+"];
 
-while (arr.length > 1){
-    let a = arr.indexOf("+"); //add index
-    let s = arr.indexOf("-"); //subtract index
-    let m = arr.indexOf("*"); //mult. index
-    let d = arr.indexOf("/"); //div. index
+function solve(arr){
+    while (arr.length > 2){
+        let a = arr.indexOf("+"); //add index
+        let s = arr.indexOf("-"); //subtract index
+        let m = arr.indexOf("*"); //mult. index
+        let d = arr.indexOf("/"); //div. index
 
-    if (m >= 0 && d >= 0){
-        if (m < d){
+        if (m >= 0 && d >= 0){
+            if (m < d){
+                arr.splice(m - 1, 3, operate(arr[m], arr[m - 1], arr[m + 1]));
+            } else {
+                arr.splice(d - 1, 3, operate(arr[d], arr[d - 1], arr[d + 1]));
+            }
+        } else if (m >= 0){
             arr.splice(m - 1, 3, operate(arr[m], arr[m - 1], arr[m + 1]));
-        } else {
+        } else if (d >= 0){
             arr.splice(d - 1, 3, operate(arr[d], arr[d - 1], arr[d + 1]));
+        } else if (a >= 0){
+            arr.splice(a - 1, 3, operate(arr[a], arr[a - 1], arr[a + 1]));
+        } else if (s >=0){
+            arr.splice(s - 1, 3, operate(arr[s], arr[s - 1], arr[s + 1]));
         }
-    } else if (m >= 0){
-        arr.splice(m - 1, 3, operate(arr[m], arr[m - 1], arr[m + 1]));
-    } else if (d >= 0){
-        arr.splice(d - 1, 3, operate(arr[d], arr[d - 1], arr[d + 1]));
-    } else if (a >= 0){
-        arr.splice(a - 1, 3, operate(arr[a], arr[a - 1], arr[a + 1]));
-    } else if (s >=0){
-        arr.splice(s - 1, 3, operate(arr[s], arr[s - 1], arr[s + 1]));
+        console.log(arr);
     }
-    console.log(arr);
-    run = false;
+    return arr;
+}
+console.log(solve(arr));
+
+
+let equation = [];
+
+const buttons = document.querySelectorAll("button");
+const backspace = document.querySelector(".backspace");
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
+const display = document.querySelector(".display");
+const input = document.querySelector(".input");
+
+
+/*
+Won't work as written, need:
+    1. number presses to add to a string and update input textContent
+    
+    2. event listeners for operators to add string then operator to 
+    equation arr, clear string
+*/
+for (let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", function(){
+        console.log(buttons[i].textContent);
+        if (buttons[i] == backspace && equation.length > 0){
+            equation.pop();
+            display.textContent = equation.join(" ");
+            input.textContent = "";
+        } else if (buttons[i] == clear){
+            equation = [];
+            display.textContent = equation.join(" ");
+            input.textContent = "";
+        } else if (buttons[i] == equal){
+            let result = solve(equation);
+            input.textContent = result[0];
+            equation = result[0];
+        } else {
+            equation.push(buttons[i].textContent);
+        }
+    });
 }
